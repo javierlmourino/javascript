d18fc15ab6cc815715c8f8fa7a42122466fd0bb1
Javier Leandro Mourino <javierlmourino@gmail.com>
2022-08-04 18:57:52 -0300

    events

--- testscript2.js
+++ testscript2.js
@@ -4,3 +4,16 @@ var clicks = 0;
 buttonact.onclick = () => {console.log("Item has been succcessfully added to your shopping cart"); clicks += 1;
 document.getElementById("clicks").innerHTML = clicks; };
 
+let buttonact2 = document.getElementById ("buttonact2"); 
+
+buttonact2.onclick = () => {console.log("All items have been removed from your basket"); clicks = 0; document.getElementById("clicks").innerHTML = clicks;};
+
+var cbdimg = document.getElementById ("cbdimg");
+
+cbdimg.addEventListener("mouseover", function() {
+    this.style.opacity = "40%";
+})
+
+cbdimg.addEventListener("mouseout", function() {
+    this.style.opacity = "100%";
+})
